8ca81bbb4b7412241c60c0b70d56b46b947eb3b4
Prem Suman <prem.suman@gdn-commerce.com>
2021-01-14 19:05:16 +0530

    Added functionality for setting data in existing LiveData

--- ezra-lib/src/main/java/com/prembros/ezra/repository/BaseRepositoryImpl.kt
+++ ezra-lib/src/main/java/com/prembros/ezra/repository/BaseRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.prembros.ezra.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import com.prembros.ezra.model.ApiResponse
 import kotlinx.coroutines.Dispatchers
@@ -11,7 +13,7 @@ import timber.log.Timber
  *
  * - Format for making network calls:
  * ```
- * fun someApiCallInRepositoryImpl(arg: Type) = makeApiCallFor {
+ * fun someApiCallInRepositoryImpl(arg: Type) = makeApiCall {
  *   responseOf {
  *     service.apiCall(arg)
  *   }
@@ -19,7 +21,7 @@ import timber.log.Timber
  * ```
  * - For making network calls with an action after API response:
  * ```
- * fun someApiCallInRepositoryImpl(arg: Type) = makeApiCallFor {
+ * fun someApiCallInRepositoryImpl(arg: Type) = makeApiCall {
  *   responseOf {
  *     service.apiCall(arg)
  *   }.withTransform {
@@ -27,11 +29,26 @@ import timber.log.Timber
  *   }
  * }
  * ```
+ * - If you want to pass data in your own [LiveData]:
+ * ```
+ * fun someApiCallInRepositoryImpl(arg: Type, liveData: MutableLiveData<...>) = makeApiCall(liveData) {
+ *   responseOf {
+ *     service.apiCall(arg)
+ *   }
+ * }
+ * ```
  */
 abstract class BaseRepositoryImpl {
 
-  protected fun <DATA : Any, ERROR : Any> makeApiCallFor(apiCall: suspend () -> ApiResponse<DATA, ERROR>): LiveData<ApiResponse<DATA, ERROR>> =
+  protected fun <DATA : Any, ERROR : Any> makeApiCallFor(
+    optionalLiveData: MutableLiveData<ApiResponse<DATA, ERROR>>? = null, apiCall: suspend () -> ApiResponse<DATA, ERROR>
+  ): LiveData<ApiResponse<DATA, ERROR>> = optionalLiveData?.also { provided ->
+    val result = liveData(Dispatchers.IO) { emit(apiCall()) }
+    val observer: (ApiResponse<DATA, ERROR>) -> Unit = { provided.value = it }
+    result.observeOnce(observer)
+  } ?: run {
     liveData(Dispatchers.IO) { emit(apiCall()) }
+  }
 
   protected suspend fun <DATA : Any, ERROR : Any> responseOf(call: suspend () -> ApiResponse<DATA, ERROR>): ApiResponse<DATA, ERROR> = try {
     call()
@@ -42,4 +59,11 @@ abstract class BaseRepositoryImpl {
 
   protected fun <DATA : Any, ERROR : Any> ApiResponse<DATA, ERROR>.withTransform(action: ApiResponse<DATA, ERROR>.() -> Unit): ApiResponse<DATA, ERROR> =
     apply(action)
+
+  private fun <T : Any> LiveData<T>.observeOnce(observer: Observer<T>) = observeForever(object : Observer<T> {
+    override fun onChanged(t: T?) {
+      observer.onChanged(t)
+      removeObserver(this)
+    }
+  })
 }
